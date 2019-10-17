ef743e672d822b40c458ef3cab841d0c723916ea
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-10-17 18:15:40 +0530

    First Commit

---
+++ SpringBootAppUserAdmin/src/main/java/com/tavisca/SpringBootAppUserAdmin/SpringBootAppUserAdminApplication.java
@@ -0,0 +1,13 @@
+package com.tavisca.SpringBootAppUserAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppUserAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppUserAdminApplication.class, args);
+	}
+
+}
